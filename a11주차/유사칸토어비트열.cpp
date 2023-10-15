1b853b61ba90e98334e2fb11893c900062634c0d
profrog <ache159@naver.com>
2023-10-15 23:48:56 +0900

    Update 유사칸토어비트열.cpp

--- a11주차/유사칸토어비트열.cpp
+++ a11주차/유사칸토어비트열.cpp
@@ -1,42 +1,38 @@
-#include <string>
-#include <cmath>
 #include <vector>
-#include <iostream>
-
+#include <cmath>
 using namespace std;
 vector<int> data_list = { 0,1,2,2,3 };
 
 
 int solution(int n, long long l, long long r) {
+    
     long long answer = pow(4, n);
-
-    long long l0 = l - 1;
-    long long r0 = r - 1;
-
     long long remind = pow(5, n - 1);
     long long multiple = pow(4, n - 1);
     int baseline = 4;
 
+    --l;
+    --r;
     while (remind)
     {
-        long long l1 = l0 / remind;
-        long long r1 = baseline - r0 / remind;
+        long long l1 = l / remind;
+        long long r1 = baseline - (r/ remind);
 
         answer -= multiple * (data_list[l1] + data_list[r1]);
-  
+
         if (l1 == 2)
         {
-            l0 = 0;
+            l = 0;
         }
 
         if (r1 == 2)
         {
             baseline = 0;
-            r0 = 0;
+            r = 0;
         }
 
-        l0 %= remind;
-        r0 %= remind;
+        l %= remind;
+        r %= remind;
         multiple /= 4;
         remind /= 5;
     }
