472a3f6a06d879717272fbbf32acae51e0e14816
profrog <ache159@naver.com>
2024-01-21 17:00:31 +0900

    Update 틱택토.cpp

--- 3주차/틱택토.cpp
+++ 3주차/틱택토.cpp
@@ -5,55 +5,91 @@
 
 using namespace std;
 const int size0 = 3;
+vector<int> check_o_x(3,0);
+vector<int> win_concept(6, 0);
+int mark_count = 0;
+char o_mark = 'O';
+char x_mark = 'X';
 
-int check_b(char alpa)
+int check_m(char alpa)
 {
-    if (alpa == 'O') return 1;
-    else if (alpa == 'X') return -1;
-    else return 0;
+    if (alpa == o_mark) return 1; //all collect 3
+    else if (alpa == x_mark) return -1; //all collect -3
+    return 0;
 }
 
+bool count_consist(char alpa)
+{
+    int check_con = check_o_x[2] - check_o_x[0];
+    //cout << alpa << " " << check_con;
+    if (alpa == o_mark && check_con == 1) return true; //o_mark를 둘 차례일땐(홀수) o의 개수가 하나 더 많아야 함
+    else if (alpa == x_mark && check_con == 0 ) return true; //x_mark를 둘 차례일땐(짝수) o와 x의 개수가 같아야함
+    return false;
+}
+
+bool win_consist()
+{
+    int o_win_count = 0;
+    int x_win_count = 0;
+
+    for (int i = 0; i < win_concept.size(); ++i)
+    {
+        if (win_concept[i] == 3)
+        {
+            if (count_consist(o_mark) == false)
+                return false;
+            ++o_win_count;
+        }
+
+        else if (win_concept[i] == -3)
+        {
+            if (count_consist(x_mark) == false)
+                return false;
+            ++x_win_count;
+        }
+
+        if (o_win_count * x_win_count)
+            return false; 
+    }
+
+    return true;
+}
+
+
 int solution(vector<string> board) {
-    int answer = 0;
-    
-    int _num = 0;
-    int x_num[4] = { 0 };
-    int y_num[4] = { 0 };
-    int check_x_o[2] = {0};
-  
+    int index_exch = 0;
+
     for (int y = 0; y < size0; ++y)
     {
         for (int x = 0; x < size0; ++x)
         {
-            int cur = check_b(board[y][x]);
-            int cur_in = 3 * y + x;
+            index_exch = 1 + check_m(board[y][x]); // o->2, x -> 0
 
-            y_num[cur_in / 3] += cur;
-            x_num[cur_in % 3] += cur;
-            _num += cur;
+            if (index_exch == 1) continue;
+            
+            else
+            {
+                ++check_o_x[index_exch];
+                ++mark_count;
+
+                win_concept[x] += check_m(board[y][x]); //x축 방향에 대한 score 기록
+                win_concept[y + 3] += check_m(board[y][x]); //y축 방향에 대한 score 기록
+            }
         }
     }
-    
-    x_num[3] = check_b(board[0][2]) + check_b(board[1][1]) + check_b(board[2][0]);
-    y_num[3] = check_b(board[0][0]) + check_b(board[1][1]) + check_b(board[2][2]);
-    
-    if (_num < 0 || _num > 1) return 0; 
-    check_x_o[_num] = 1;
 
-    for (int i = 0; i <= size0; ++i) //가로세로에 대하여
-    {       
-        if(abs(x_num[i])/3)
-        {  
-            if(check_x_o[(x_num[i]/3 + 1)/2] == 0) return 0;
-        }
+    win_concept.push_back(check_m(board[0][2]) + check_m(board[1][1]) + check_m(board[2][0])); // 오른쪽 위방향에 대한 score 기록
+    win_concept.push_back(check_m(board[0][0]) + check_m(board[1][1]) + check_m(board[2][2])); // 왼쪽 위방향에 대한 score 기록 
 
-        if(abs(y_num[i])/3)
-        {
-            if(check_x_o[(y_num[i]/3 + 1)/2] == 0) return 0;
-        }
+    if (mark_count > 0)
+    {
+        char alpa = ((mark_count % 2) == 0)? x_mark : o_mark;
+        if (count_consist(alpa) == false) //전체 개수에 대한 무결성
+            return 0;
+
+        if (win_consist() == false)
+            return 0; 
     }
+
     return 1;
 }
-
-
-
