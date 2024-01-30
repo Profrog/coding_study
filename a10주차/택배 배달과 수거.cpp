ffd1fc42a72531178998d6ca6aa99c4fc2f21ca1
profrog <ache159@naver.com>
2024-01-31 00:13:18 +0900

    Update 택배 배달과 수거.cpp

--- a10주차/택배 배달과 수거.cpp
+++ a10주차/택배 배달과 수거.cpp
@@ -1,49 +1,72 @@
 #include <string>
 #include <vector>
 #include <iostream>
-
 using namespace std;
+
+int sendorget[2] = { 0,0 };
+int vector_check(vector<int>& vector0) //1.작업(배송|수거) 하지 않아도 되는 집 고려대상에서 제거
+{
+    while (vector0.back() == 0)
+    {
+        vector0.pop_back();
+    }
+    return vector0.size() - 1;
+}
+
+int making_course(int cap_index, vector<int>& house, vector<int>& dummy) 
+{
+    int index0 = house.size() -1;
+    if (&house != &dummy)
+    {
+       making_course(1 - cap_index, dummy, dummy); //2.멀리 떨어진 집 부터 방문, 최대 cap 크기만큼 작업(배송과 수거는 독립적으로 고려가능)
+    }
+
+    while (sendorget[cap_index] >= 0 && index0 >= 0)
+    {
+        sendorget[cap_index] -= house[index0];
+        house[index0] = 0;
+
+        if (sendorget[cap_index] >= 0)
+        {
+            if (--index0 < 0) return -1;
+        }
+
+        else
+        {
+            house[index0] -= sendorget[cap_index];
+            break;
+        }
+    }
+
+    return index0;
+}
 
 long long solution(int cap, int n, vector<int> deliveries, vector<int> pickups) {
     long long answer = 0;
-    long long truck = cap;
-   
-    for(int i = 1 ; i < n ; ++i)
+    int sending_index = deliveries.size() - 1;
+    int getting_index = pickups.size() - 1;
+
+    while (sending_index >= 0 || getting_index >= 0) 
     {
-        while (deliveries.back() > 0)
-        {
-            answer += deliveries.size();
-            deliveries.back() -= cap;
-            pickups.back() -= cap;
+        sending_index = vector_check(deliveries);
+        getting_index = vector_check(pickups);
+        if (sending_index < 0 && getting_index < 0) break; //3.모든 집이 작업 대상이 아닐 때 이동거리 합 반환
+
+        sendorget[0] = cap;
+        sendorget[1] = cap;
+
+        if (sending_index >= getting_index) //2.멀리 떨어진 집 부터 방문, 최대 cap 크기만큼 작업(배송과 수거는 독립적으로 고려가능)
+        {           
+            answer += (sending_index + 1) * 2;
+            making_course(0, deliveries, pickups);
         }
 
-        while (pickups.back() > 0)
+        else
         {
-            answer += pickups.size();
-            deliveries.back() -= cap;
-            pickups.back() -= cap;
+            answer += (getting_index + 1) * 2;
+            making_course(1, pickups, deliveries);
         }
-
-        deliveries[deliveries.size() - 2] += deliveries.back();
-        deliveries.pop_back();
-        
-        pickups[pickups.size() - 2] += pickups.back();
-        pickups.pop_back();
     }
 
-    while (deliveries.back() > 0)
-    {
-        answer += deliveries.size();
-        deliveries.back() -= cap;
-        pickups.back() -= cap;
-    }
-
-    while (pickups.back() > 0)
-    {
-        answer += pickups.size();
-        deliveries.back() -= cap;
-        pickups.back() -= cap;
-    }
-
-    return answer*2;
+    return answer;
 }
