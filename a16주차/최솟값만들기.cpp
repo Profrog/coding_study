5cdf347b41ba24c8ede3ccd45e473392e7b2400b
profrog <ache159@naver.com>
2024-02-03 15:55:38 +0900

    Update 최솟값만들기.cpp

--- a16주차/최솟값만들기.cpp
+++ a16주차/최솟값만들기.cpp
@@ -8,7 +8,7 @@ int solution(vector<int> A, vector<int> B)
     int answer_1 = 0;
 
     sort(A.begin(), A.end());
-    sort(B.begin(), B.end(), greater<int>());
+    sort(B.begin(), B.end(), greater<int>()); //1.A리스트를 최솟값 순으로 정렬, B리스트를 최댓값 순으로 정렬하여 각 원소를 곱하여 반환한다.
 
     for (int i = 0; i < A.size(); ++i)
     {
