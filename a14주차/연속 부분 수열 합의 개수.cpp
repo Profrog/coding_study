1a24a1eb8de455460c0fefef293735f4f4ef0483
profrog <ache159@naver.com>
2024-02-03 15:05:04 +0900

    Update 연속 부분 수열 합의 개수.cpp

--- a14주차/연속 부분 수열 합의 개수.cpp
+++ a14주차/연속 부분 수열 합의 개수.cpp
@@ -20,7 +20,7 @@ void make_shift(vector<int>& elements, int index)
 
         int beta = alpa + elements[(i + index) % elements.size()];
 
-        if (!memory[beta])
+        if (!memory[beta]) //2.부분 수열의 길이의 합이 기존에 저장 되었던 경우는 합의 가짓수에서 제외한다.
         {
             memory[beta] = 1;
             ++answer;
@@ -35,7 +35,7 @@ int solution(vector<int> elements) {
 
     shift.assign(elements.size(), 0);
 
-    for (int i = 0; i < elements.size(); ++i)
+    for (int i = 0; i < elements.size(); ++i) //1. 부분 수열의 길이(1~n)에 따라 수열의 시작포인트를 한칸씩 shift하면서 합을 구한다
     {
         make_shift(elements, i);
     }
