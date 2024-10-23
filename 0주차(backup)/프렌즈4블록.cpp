fc2e56ddc2981fd92e2dabf485920e12f2b8ff85
profrog <ache159@naver.com>
2024-10-23 10:42:46 +0900

    Update 프렌즈4블록.cpp

--- 0주차(backup)/프렌즈4블록.cpp
+++ 0주차(backup)/프렌즈4블록.cpp
@@ -1,21 +1,25 @@
 #include <string>
 #include <vector>
-#include <list>
 #include <iostream>
 #include <algorithm>
 using namespace std;
 
 vector<string> map0;
+vector<pair<int,int>> del_trg;
+
 int row = 0;
 int col = 0;
+const int mark_chk = 32;
+const int max_mark = 95;
+const char space0 = ' ';
 
 void viewer()
 {
     cout << endl;
     
-    for(int y = col-1 ; y >= 0; --y)
+    for(int y = 0 ; y <= col-1 ; ++y)
     {
-        for(int x = row-1 ; x >=0 ; --x) //row check
+        for(int x = 0 ; x <= row-1 ; ++x) //row check
         {
             cout << map0[y][x];       
         }
@@ -29,8 +33,9 @@ void viewer()
 bool compareMark(char input, char mark)
 {
     if(input == mark) return true;
-    else if(input == (mark+30)) return true;
-    return false;
+    else if(input == (mark+mark_chk)) return true;
+    else if((input+mark_chk) == mark) return true;
+    else return false;
 }
 
 
@@ -46,88 +51,66 @@ bool isDeleteblock(int y, int x, int y_vec, int x_vec, char mark)
 
     if(compareMark(map0[y][x1],mark) && compareMark(map0[y1][x],mark) && compareMark(map0[y1][x1],mark))
     {
+        if(map0[y][x] < max_mark)
+        {
+            map0[y][x] += mark_chk;
+        }
+        
+        if(map0[y][x1] < max_mark)
+        {
+            map0[y][x1] += mark_chk;
+        }
+           
+        if(map0[y1][x] < max_mark)
+        {
+            map0[y1][x] += mark_chk;
+        }
+        
+        if(map0[y1][x1] < max_mark)
+        {
+            map0[y1][x1] += mark_chk;
+        }
+         
         return true;
     }
         
-    return false;
-}
-
-int findDeleteBlock(int y, int x,int volume,char mark)
-{
-    if(isDeleteblock(y-1,x,-1,-1,mark))
-    {
-       volume += findDeleteBlock(y-1,x,2,mark);
-    }
-    
-    if(isDeleteblock(y-1,x-1,-1,-1,mark))
-    {
-       volume += findDeleteBlock(y-1,x-1,3,mark);
-    }
-    
-    if(isDeleteblock(y,x-1,-1,-1,mark))
-    {
-       volume += findDeleteBlock(y,x-1,2,mark);
-    }
-    
-    map0[y][x] = mark + 30;
-    map0[y][x-1] = mark + 30;
-    map0[y-1][x] = mark + 30;
-    map0[y-1][x-1] = mark + 30;
-    //cout << y << "#" << x << " "<< volume << endl;
-    return volume;
+   else return false;
 }
 
 
 int mapControl()
 {
     int answer = 0;
+    for(int y = 0 ; y < col-1; ++y)
+    {
+        for(int x = 0 ; x < row -1 ;++x)
+        { 
+            if(map0[y][x] == space0) break;
+            else isDeleteblock(y,x,1,1,map0[y][x]);           
+        }
+    }
+    
+     for(int y = 0 ; y <= col-1; ++y)
+    {
+ 
+        for(int x = row-1 ; x >= 0 ;--x)
+        {
+           if(map0[y][x] > max_mark && map0[y][x] != space0)
+           {
+                map0[y][x] = space0;
+                ++answer;
+                int cur_idx = x;
+                int new_idx = x+1;                
+                while(map0[y][new_idx] != space0 && (new_idx < row))
+                {
+                    swap(map0[y][cur_idx],map0[y][new_idx]);
+                    cur_idx = new_idx;
+                    ++new_idx;
+                } 
+           }
+        }   
+    }
 
-    
-    for(int x = row-1 ; x >= 0; --x)
-    {
-        for(int y = col-1 ; y >=0; --y) //row check
-        {
-    
-            if(map0[y][x] == '#')
-            {
-                continue;
-            }
-            
-            else if(map0[y][x] > 90)
-            {
-                continue;
-            }
-            
-            else if(isDeleteblock(y,x,-1,-1,map0[y][x]))
-            {
-                int data = findDeleteBlock(y,x,4,map0[y][x]);
-                //cout << data << " ";
-            }            
-        }
-        
-        //cout << endl;
-    }
-    
-    for(int y = 0 ; y < col;++y)
-    {
-        for(auto elec = map0[y].begin(); elec != map0[y].end();)
-        {
-            if(*elec > 90)
-            {
-                elec = map0[y].erase(elec);
-                map0[y].push_back('#');
-                ++answer; 
-            }
-            
-            else
-            {
-                ++elec;
-            }
-        }
-    }
-    
-    //viewer();
-    //cout << answer << " " << endl;
     return answer;
 }
 
@@ -136,29 +119,22 @@ int solution(int m, int n, vector<string> board) {
     int answer = 0;
     row = m;
     col = n;
-    for(int x = col-1 ; x >= 0; --x)
+    for(int x = 0 ; x <= col-1; ++x)
     {
         string alpa = "";
-        for(int y = row-1 ; y >= 0 ; --y)
+        for(int y = row-1; y >= 0 ; --y)
         {
             alpa += board[y][x];
         }    
         map0.push_back(alpa);
     }
     
-    //viewer();
-    
     int data = 0;
     do{       
         data = mapControl();
         answer += data;
-        cout << endl;
         
     }while(data > 0);
 
     return answer;
 }
-
-
-
-
